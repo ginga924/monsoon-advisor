41aea871fac0814255c17e02a54ad9e5825a8c95
Phaninthorn Swanyawatthaga <136943349+ginga924@users.noreply.github.com>
2024-11-01 06:02:20 +0000

    dascz

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ def generate_predictions(game_data, forecast_start_day, forecast_end_day, trust_
     forecast['adjusted_yhat'] = forecast['adjusted_yhat'].clip(lower=1)
     return forecast[['ds', 'yhat', 'adjusted_yhat']]
 
-# Retrieve the last 14 days of sales data with complete game days from COM_day table
+# Retrieve all available sales data with complete game days from COM_day table
 def get_game_data(host, port, database, user, password):
     try:
         conn = mysql.connector.connect(
@@ -150,12 +150,12 @@ def get_game_data(host, port, database, user, password):
 
     table_name = f"LN{user.split('_')[-1]}_sales"
     try:
-        # Query to get all game dates from COM_day
-        com_day_query = "SELECT date FROM COM_day ORDER BY date DESC LIMIT 48;"
+        # Query to get all game dates from COM_day without limit
+        com_day_query = "SELECT date FROM COM_day ORDER BY date DESC;"
         com_day_df = pd.read_sql_query(com_day_query, conn)
 
-        # Query to get last 14 days of sales data from sales table
-        sales_query = f"SELECT date, unit_sold FROM {table_name} ORDER BY date DESC LIMIT 48;"
+        # Query to get all sales data from sales table without limit
+        sales_query = f"SELECT date, unit_sold FROM {table_name} ORDER BY date DESC;"
         sales_df = pd.read_sql_query(sales_query, conn)
 
         # Full join sales data with COM_day dates on 'date' column
